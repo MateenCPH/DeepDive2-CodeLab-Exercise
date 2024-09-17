08c1a857a6dc2c86673cb5a7e19e639384f09e59
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2024-09-17 11:57:27 +0200

    delete,findById and getSet done in dao

--- src/main/java/dat/daos/MovieDAO.java
+++ src/main/java/dat/daos/MovieDAO.java
@@ -1,32 +1,62 @@
 package dat.daos;
 
 import dat.dtos.MovieDTO;
+import dat.entities.Movie;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceException;
 
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
-public class MovieDAO implements IDAO<MovieDTO>{
+public class MovieDAO implements IDAO<Movie> {
+
+    private EntityManagerFactory emf;
+
+    public MovieDAO(EntityManagerFactory emf) {
+        this.emf = emf;    }
+
+
     @Override
-    public MovieDTO create(MovieDTO movieDTO) {
+    public Movie create(Movie movie) {
         return null;
     }
 
     @Override
-    public MovieDTO update(MovieDTO movieDTO) {
+    public Movie update(Movie movie) {
         return null;
     }
 
     @Override
-    public void delete(MovieDTO movieDTO) {
-
+    public void delete(Movie movie) {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.remove(movie);
+            em.getTransaction().commit();
+        } catch (PersistenceException e) {
+            System.out.println("Error while deleting movie");
+            return;
+        }
     }
 
     @Override
-    public MovieDTO getById(Long id) {
-        return null;
+    public Movie getById(Long id) {
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.find(Movie.class, id);
+        } catch (PersistenceException e) {
+            System.out.println("Error while getting Movie by id");
+            return null;
+        }
     }
 
     @Override
-    public Set<MovieDTO> getAll() {
-        return Set.of();
+    public Set<Movie> getAll() {
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.createQuery("select m FROM Movie m", Movie.class).getResultStream().collect(Collectors.toSet());
+        } catch (PersistenceException e) {
+            System.out.println("Error while getting Movie list");
+            return null;
+        }
     }
 }
